99f906e7eaa456be08dfcbc56bedee964735cb36
Jeff-Peters513 <petersjeff288@gmail.com>
2022-11-09 15:19:09 -0500

    add dtos and login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,8 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Cryptography;
 using System.Text;
+using API.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,12 +21,17 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+            
+            if (await UserExists(registerDto.UserName)){
+                return BadRequest("Username is taken");
+            }
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser {
-                UserName = username, 
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(), 
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             
@@ -32,6 +39,31 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;           
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+
+            if(user == null) {return Unauthorized("Invalid username");}
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+                
+            }
+            return user;       
+        }
+
+
+        private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
 
     }
